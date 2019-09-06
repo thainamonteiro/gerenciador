27f4b5005ebf2eb97dbaf5e3248e1d32664841b7
Thainá <thainamonteiro60@unigranrio.br>
2019-09-06 15:03:17 -0300

    Add list

--- src/br/com/alura/gerenciador/servlet/Banco.java
+++ src/br/com/alura/gerenciador/servlet/Banco.java
@@ -6,9 +6,18 @@ import java.util.List;
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<>();
+	
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setNome("Alura");
+		Empresa empresa2 = new Empresa();
+		empresa.setNome("Caelum");
+		lista.add(empresa);
+		lista.add(empresa2);
+	}
 
 	public void adiciona(Empresa empresa) {
-		lista.add(empresa);
+		Banco.lista.add(empresa);
 	}
 
 	public List<Empresa> getEmpresa() {
